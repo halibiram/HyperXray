0cbb5d98e32715849729c5c963eac1fae05883e0
Halil ibrahim Özdemir <94225244+halibiram@users.noreply.github.com>
2025-11-12 12:54:47 +0300

    Update build configuration and Gradle settings

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -15,7 +15,7 @@
 
 APP_OPTIM := release
 APP_PLATFORM := android-21
-APP_ABI := arm64-v8a
+APP_ABI := arm64-v8a x86_64
 APP_CFLAGS := -O3 -DPKGNAME=com/hyperxray/an
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
